eff52f3ef84b30cdb5a23180ef68155d74488a0c
xie0619 <1657098617@qq.com>
2021-11-02 19:02:51 +0800

    fix:修复代码的不完整性

--- src/index.py
+++ src/index.py
@@ -8,9 +8,9 @@ def main_handler(event,context):
     api_id = os.environ.get("APP_ID").split(";")  #输入api_id，一个账号一项，以分号隔开
     api_hash = os.environ.get("API_HASH").split(";")  #输入api_hash，一个账号一项，和上面的 api_id要一一对应，以分号隔开
     auth_string = os.environ.get("AUTH_KEY").split(";") #以分号隔开
-    print("api_id: " + str(api_id))
-    print("api_hash: " + str(api_hash))
-    print("auth_string: " + str(auth_string))
+    # print("api_id: " + str(api_id))
+    # print("api_hash: " + str(api_hash))
+    # print("auth_string: " + str(auth_string))
     for num in range(len(api_id)):
         client = TelegramClient(StringSession(auth_string[num]), api_id[num], api_hash[num])
         client.start()
